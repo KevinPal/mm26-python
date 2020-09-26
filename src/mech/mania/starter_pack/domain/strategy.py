4d066da131e8d280cc2d58cea9a48898a04cfd26
Kevin Zheng <coffeevector@gmail.com>
2020-09-26 16:28:32 -0700

    pick up weapon if better
    Auto equip
    
    Logging
    
    Move code up

--- src/mech/mania/starter_pack/domain/strategy.py
+++ src/mech/mania/starter_pack/domain/strategy.py
@@ -6,6 +6,8 @@ from mech.mania.starter_pack.domain.model.characters.player import Player
 from mech.mania.starter_pack.domain.model.game_state import GameState
 from mech.mania.starter_pack.domain.api import API
 
+from mech.mania.starter_pack.domain.model.items.weapon import Weapon
+
 
 class Strategy:
     def __init__(self, memory):
@@ -29,6 +31,49 @@ class Strategy:
         self.logger.info("In make_decision")
         self.logger.info(f"Currently at position: ({self.curr_pos.x},{self.curr_pos.y}) on board '{self.curr_pos.board_id}'")
 
+        last_action, type = self.memory.get_value("last_action", str)
+
+        if last_action is not None and last_action == "PICKUP":
+            self.logger.info("Last action was picking up, equipping picked up object")
+            self.memory.set_value("last_action", "EQUIP")
+            return CharacterDecision(
+                decision_type="EQUIP",
+                action_position=None,
+                action_index=0  # self.my_player.get_free_inventory_index()
+            )
+
+        weapon = self.my_player.get_weapon()
+        # deciding to pick up item
+        try:
+            pos_with_items = self.find_positions_with_items_in_range(self.curr_pos, 10)
+            self.logger.info(f"number of item drops i see is {len(pos_with_items)}")
+
+            # if weapon has better attack
+            for pos in pos_with_items:
+                items = self.game_state.get_board(self.curr_pos.get_board_id()).get_tile_at(pos).get_items()
+                for item in items:
+                    self.logger.info(f"I can seee an item {item}")
+                    if isinstance(item, Weapon):
+                        if item.get_attack() > weapon.get_attack():
+                            if pos.get_x() == self.curr_pos.get_x() and pos.get_y() == self.curr_pos.get_y():
+                                self.logger.info(f"Standing on weapon with attack {item.get_attack()}, picking up")
+                                self.memory.set_value("last_action", "PICKUP")
+                                return CharacterDecision(
+                                    decision_type="PICKUP",
+                                    action_position=None,
+                                    action_index=0
+                                )
+                            else:
+                                self.logger.info(f"Found weapon with attack {item.get_attack()}, approaching")
+                                self.memory.set_value("last_action", "MOVE")
+                                return CharacterDecision(
+                                    decision_type="MOVE",
+                                    action_position=self.find_position_to_move(self.my_player, pos),
+                                    action_index=0
+                                )
+        except Exception as e:
+            self.logger.warn(str(e))
+
         monster_list = self.crappy_find_enemies_by_distance(self.curr_pos, name_filter="slime")
         close_mon = monster_list[0]
         dist = self.curr_pos.manhattan_distance(close_mon.get_position())
@@ -36,26 +81,12 @@ class Strategy:
 
         decision = None
 
-        try:
-            portals = self.game_state.get_board(self.curr_pos.get_board_id()).get_portals()
-            self.logger.warn(str(portals))
-            self.logger.warn(len(portals))
-        except Exception as e:
-            self.logger.warn(str(e))
 
-        try:
-            grid = self.game_state.get_board(self.curr_pos.get_board_id()).grid
-            all_items = ", ".join([ ", ".join([str(type(item)) for item in tile.get_items()]) for row in grid for tile in row ])
-            self.logger.warn(all_items)
-        except Exception as e:
-            self.logger.warn(str(e))
-        #self.logger.warn(f"items in range: {self.find_positions_with_items_in_range(self.curr_pos, 10)}")
-
-        weapon = self.my_player.get_weapon()
         self.logger.warn("Weapon range: %d, attack %d" % (weapon.get_range(), weapon.get_attack()))
 
         if(dist > weapon.get_range()):
-            new_pos = self.crappy_find_position_to_move(self.my_player, close_mon.get_position())
+            new_pos = self.find_position_to_move(self.my_player, close_mon.get_position())
+            self.memory.set_value("last_action", "MOVE")
             decision = CharacterDecision(
                 decision_type="MOVE",
                 action_position=new_pos,
@@ -63,6 +94,7 @@ class Strategy:
             )
             self.logger.warn("Moving from (%d %d) => (%d %d)" % (self.curr_pos.x, self.curr_pos.y, new_pos.x, new_pos.y))
         else:
+            self.memory.set_value("last_action", "ATTACK")
             decision = CharacterDecision(
                 decision_type="ATTACK",
                 action_position=close_mon.get_position(),
@@ -77,9 +109,6 @@ class Strategy:
         for i in range(-ran, ran+1):
             for j in range(-ran, ran+1):
                 position = pos.create(pos.get_x() + i, pos.get_y() + j, pos.get_board_id())
-                self.logger.warn(f"checking position for items: {position.get_x()}, {position.get_y()}")
-                self.logger.warn(self.game_state.get_board(pos.get_board_id()).grid[position.y][position.x])
-
                 if len(self.game_state.get_board(pos.get_board_id()).get_tile_at(position).get_items()) > 0:
                     positions.append(position)
         return positions
@@ -121,13 +150,18 @@ class Strategy:
 
     # feel free to write as many helper functions as you need!
     def find_position_to_move(self, player: Player, destination: Position) -> Position:
-        path = self.api.find_path(player.get_position(), destination)
-        # path can be empty if player.get_position() == destination
-        if len(path) == 0:
-            return player.get_position()
-        pos = None
-        if len(path) < player.get_speed():
-            pos = path[-1]
-        else:
-            pos = path[player.get_speed() - 1]
-        return pos
+        try:
+            path = self.api.find_path(player.get_position(), destination)
+            # path can be empty if player.get_position() == destination
+            if len(path) == 0:
+                return player.get_position()
+            pos = None
+            if len(path) < player.get_speed():
+                pos = path[-1]
+            else:
+                pos = path[player.get_speed() - 1]
+            return pos
+        except Exception as e:
+            self.logger.warn(str(e))
+            self.logger.warn("find position failed, defaulting to crappy find_position")
+            return self.crappy_find_position_to_move(player, destination)
